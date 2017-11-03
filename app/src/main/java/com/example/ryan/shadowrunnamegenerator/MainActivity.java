47d7be2422f6079c9c9c15ffd7a910472c0c6e1f
RyanSilver <silverseaturtle@hotmail.com>
2017-11-03 03:33:04 -0400

    changed names and added model and controller classes.  Model works and is ready for testing. todo: first island: get names printing on screen after selecting context from menus second: fix inevitable problems with back button and implementation third: add DB support forth: add firebase and Ads

--- app/src/main/java/com/example/ryan/shadowrunnamegenerator/MainActivity.java
+++ app/src/main/java/com/example/ryan/shadowrunnamegenerator/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.ryan.shadowrunnamegenerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView listView;
+    private ListView displayListView;
+    private ArrayList<String> listcontents;
+    public static nameGenController controller;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        controller=new nameGenController();
+        if (savedInstanceState!=null){
+            //reload the list view from memory
+            listcontents.clear();
+
+        }
+        else{
+            //initialize the list
+
+        }
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState){
+        savedInstanceState.putString("listItem0",listcontents.get(0));//...
+
+    }
+
+
 }
